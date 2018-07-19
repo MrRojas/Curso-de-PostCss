f0f6d94b021b507a079541adac46d4c380e586b5
= <=>
2018-07-19 12:57:55 -0400

    iniciando proyecto

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+// Invocar Gulp
+var gulp = require('gulp')
+// Invocar Postcss con gulp 
+var postcss = require('gulp-postcss')
+//Invocar browserSync
+
+var browserSync = require('browser-sync')
+
+// servidor de Desarrollo con Gulp
+// task son las tareas a ejecutar en la cual se pasa un nombre y una funcion 
+gulp.task( 'serve' , () => {
+
+	// llamar a browserSync
+	// se pasa un objeto de configuracion 
+	browserSync.init({
+		// configurar donde arranca el server 
+		server: {
+			baseDir : './dist'
+		}
+	})
+
+})
+
+// tarea para procesar CSS 
+
+gulp.task('css' , () =>{
+
+	// array con los plugins de postcss que debemos instalar con anterioridad 
+	var plugins = []
+
+	// primero lee todo los que se pasa en src, en este caso todo lo que sea .css  
+	return gulp.src('./src/*.css')
+	// luego asignamos pipe (tuberias) para que se mueve en el directorio con la ayuda de node_js 
+	// al cual al primero le asignamos postcss pasando el array de plugins
+	.pipe(postcss(plugins))
+	// una segunda tuberia en el cual le decimos a gulp donde dejar los archivos procesados 
+	.pipe(gulp.dest('./dist/css'))
+	// y asignamos un ultimo pipe donde le decimos a browserSync que recargue 
+	.pipe(browserSync.stream())
+
+})
+
+// tarea para vigilar los cambios 
+
+gulp.task('watch' , () => {
+	// vigilar los cambios en css
+	// 1er parametro es la ruta del archivo a vigilar 
+	// 2do parametro un array con las tareas a ejecutar 
+	gulp.watch('./src/*.css' , ['css'] )
+	// vigila cuando surja un cambio en el html 
+	// browserSync.reload es para recargar el navegador 
+	gulp.watch('./dist/*.html').on('change' , browserSync.reload )
+})
+
+// Por ultimo 
+
+// Una tarea que llama al arreglo de tareas a ejecutar 
+
+gulp.task('default' , ['css' , 'serve'] )
